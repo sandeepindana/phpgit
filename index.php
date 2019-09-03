0127360cce21566bf858abba7224991c44e4b0ab
Sandeep Indana <isandeep.1609@gmail.com>
2019-09-03 22:54:11 +0530

    commit 3-9-2019-22-54

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Git Tutorial</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <h1>Hello World...This is Git Tutorial...!</h1>
+    </body>
+</html>
